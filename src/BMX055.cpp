bc9659df91d5704fe968011fffcedfa02cda0b4c
Peter Zheng <zhengp@live.co.uk>
2024-12-12 02:32:31 +0000

    refactor for rp2040 pico-sdk

--- src/BMX055.cpp
+++ src/BMX055.cpp
@@ -1,359 +1,401 @@
 #include "BMX055.h"
+#include <cstdint> // Include fixed-width integer types (e.g., uint8_t, uint16_t)
 
-BMX055::BMX055(){}
+BMX055::BMX055(i2c_inst_t *port, uint8_t accl_addr, uint8_t gyro_addr, uint8_t magn_addr)
+{
+    // Constructor - Typically you would initialize I2C here if needed
+    i2c_port = port;
+    accl_addr_ = accl_addr;
+    gyro_addr_ = gyro_addr;
+    magn_addr_ = magn_addr;
+}
 
 uint8_t BMX055::beginAcc(char range)
 {
+    _range = range; // Save the range internally
 
-	char _range = range; //2g Range 0x03
+    switch (range)
+    {
+    case 0x03:
+        accRange = (2.0 / 2048.0);
+        break;
+    case 0x05:
+        accRange = (4.0 / 2048.0);
+        break;
+    case 0x08:
+        accRange = (8.0 / 2048.0);
+        break;
+    case 0x0C:
+        accRange = (16.0 / 2048.0);
+        break;
+    }
 
-	switch (range)
-	{
-	case 0x03:
-		accRange = (2.0 / 2048.0);
-		break;
-	case 0x05:
-		accRange = (4.0 / 2048.0);
-		break;
-	case 0x08:
-		accRange = (8.0 / 2048.0);
-		break;
-	case 0x0C:
-		accRange = (16.0 / 2048.0);
-		break;
-	}
-	// Initialise I2C communication as MASTER
-	Wire1.begin();
+    // Start I2C communication and set up the accelerometer
+    uint8_t data[] = {0x0F, range};
+    uint8_t check = 0;
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_ACCL_ADDR);
-	// Select PMU_Range register
-	Wire1.write(0x0F);
-	// Range = +/- 2g
-	Wire1.write(range);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    i2c_write_blocking(i2c_port, accl_addr_, data, 2, false);
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_ACCL_ADDR);
-	// Select PMU_BW register
-	Wire1.write(0x10);
-	// Bandwidth = 7.81 Hz
-	Wire1.write(0x08);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    data[0] = ACCL_FILTER_ADDR;
+    data[1] = 0x08; // Bandwidth = 7.81 Hz
+    i2c_write_blocking(i2c_port, accl_addr_, data, 2, false);
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_ACCL_ADDR);
-	// Select PMU_LPW register
-	Wire1.write(0x11);
-	// Normal mode, Sleep duration = 0.5ms
-	Wire1.write(0x00);
-	// Stop I2C Transmission on the device
-	Wire1.endTransmission();
+    data[0] = 0x11;
+    data[1] = 0x00; // Normal mode, Sleep duration = 0.5ms
+    i2c_write_blocking(i2c_port, accl_addr_, data, 2, false);
+
+    // data[0] = 0x36; // slow offset compensation
+    // data[1] = 0x00;
+    // i2c_write_blocking(i2c_port, accl_addr_, data, 2, false);
+    // Reading accelerometer NVM offsets
+    data[0] = 0x38; // x-axis
+    i2c_write_blocking(i2c_port, accl_addr_, &data[0], 1, true);
+    i2c_read_blocking(i2c_port, accl_addr_, &data[1], 1, false);
+    printf("Acc offset x: %d ", (int8_t) data[1]);
+
+    data[0] = 0x39; // y-axis
+    i2c_write_blocking(i2c_port, accl_addr_, &data[0], 1, true);
+    i2c_read_blocking(i2c_port, accl_addr_, &data[1], 1, false);
+    printf("y: %d ", (int8_t) data[1]);
+
+    data[0] = 0x3A; // z-axis
+    data[1] = 0xED; // z-axis -0.15g
+    i2c_write_blocking(i2c_port, accl_addr_, data, 2, false);
+    i2c_write_blocking(i2c_port, accl_addr_, &data[0], 1, true);
+    i2c_read_blocking(i2c_port, accl_addr_, &data[1], 1, false);
+    printf("z: %d \n", (int8_t) data[1]);
+
+    return 0; // Indicate success (or you could return an error code)
 }
 
-uint8_t BMX055::beginGyro()
+uint8_t BMX055::beginGyro(char resolution)
 {
+    _resolution = resolution;
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_GYRO_ADDR);
-	// Select Range register
-	Wire1.write(0x0F);
-	// Full scale = +/- 125 degree/s
-	Wire1.write(0x04);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    switch (resolution)
+    {
+    case 0x00:
+        gyroRes = 61.0 / 1000.0;
+        break;
+    case 0x01:
+        gyroRes = 30.5 / 1000.0;
+        break;
+    case 0x02:
+        gyroRes = 15.3 / 1000.0;
+        break;
+    case 0x03:
+        gyroRes = 7.6 / 1000.0;
+        break;
+    case 0x04:
+        gyroRes = 3.8 / 1000.0;
+        break;
+    }
+    uint8_t data[] = {0x0F, resolution}; // Range register, Full scale = +/- 125 degree/s
+    uint8_t check = 0x01;
+    int loop = 0;
+    printf("Gyro set resolution: 0x%02X ", data[1]);
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_GYRO_ADDR);
-	// Select Bandwidth register
-	Wire1.write(0x10);
-	// ODR = 100 Hz
-	Wire1.write(0x07);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    while ((check != data[1]) && (loop < 500))
+    {
+        i2c_write_blocking(i2c_port, gyro_addr_, data, 2, false);
+        sleep_ms(10);
+        i2c_write_blocking(i2c_port, gyro_addr_, &data[0], 1, true);
+        i2c_read_blocking(i2c_port, gyro_addr_, &check, 1, false);
+        sleep_ms(10);
+        loop++;
+        // printf("Gyro act resolution: 0x%02X\n", check);
+    }
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_GYRO_ADDR);
-	// Select LPM1 register
-	Wire1.write(0x11);
-	// Normal mode, Sleep duration = 2ms
-	Wire1.write(0x00);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    if (check != data[1])
+    {
+        printf("Failed, loop %d\n", loop);
+    }
+    else
+    {
+        printf("Success, loop %d\n", loop);
+    }
+    loop = 0;
+
+    data[0] = 0x10;
+    data[1] = 0x07; // Bandwidth = 100 Hz
+    i2c_write_blocking(i2c_port, gyro_addr_, data, 2, false);
+
+    data[0] = 0x11;
+    data[1] = 0x00; // Normal mode, Sleep duration = 2ms
+    i2c_write_blocking(i2c_port, gyro_addr_, data, 2, false);
+
+    data[0] = 0x31; // Slow Offset Cancellation
+    data[1] = 0xCF; // Enable for all axes, 1.0 deg/s, duration = 80ms
+    i2c_write_blocking(i2c_port, gyro_addr_, &data[0], 1, true);
+    i2c_read_blocking(i2c_port, gyro_addr_, &check, 1, false);
+    printf("Gyro slow cancellation pre : 0x%02X\n", check);
+
+    i2c_write_blocking(i2c_port, gyro_addr_, data, 2, false);
+
+    i2c_write_blocking(i2c_port, gyro_addr_, &data[0], 1, true);
+    i2c_read_blocking(i2c_port, gyro_addr_, &check, 1, false);
+    printf("Gyro slow cancellation post: 0x%02X\n", check);
+    
+    uint8_t gyro_offset[6] = {0x36, 0b10000000, 0b00000000, 0b11111111, 0b11111111, 0b00001111};
+    i2c_write_blocking(i2c_port, gyro_addr_, gyro_offset, 6, false);
+    // gx: 000000001011
+    // gy: 111111110001
+    // gz: 111111110001
+
+    return 0;
 }
 
 uint8_t BMX055::beginMagn()
 {
+    uint8_t data[] = {0x4B, 0x83}; // Soft reset
+    i2c_write_blocking(i2c_port, magn_addr_, data, 2, false);
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_MAGN_ADDR);
-	// Select Mag register
-	Wire1.write(0x4B);
-	// Soft reset
-	Wire1.write(0x83);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    data[0] = 0x4C;
+    data[1] = 0x00; // Normal Mode, ODR = 10 Hz
+    i2c_write_blocking(i2c_port, magn_addr_, data, 2, false);
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_MAGN_ADDR);
-	// Select Mag register
-	Wire1.write(0x4C);
-	// Normal Mode, ODR = 10 Hz
-	Wire1.write(0x00);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    data[0] = 0x4E;
+    data[1] = 0x84; // X, Y, Z-Axis enabled
+    i2c_write_blocking(i2c_port, magn_addr_, data, 2, false);
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_MAGN_ADDR);
-	// Select Mag register
-	Wire1.write(0x4E);
-	// X, Y, Z-Axis enabled
-	Wire1.write(0x84);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    data[0] = 0x51;
+    data[1] = 0x04; // No. of Repetitions for X-Y Axis = 9
+    i2c_write_blocking(i2c_port, magn_addr_, data, 2, false);
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_MAGN_ADDR);
-	// Select Mag register
-	Wire1.write(0x51);
-	// No. of Repetitions for X-Y Axis = 9
-	Wire1.write(0x04);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
+    data[0] = 0x52;
+    data[1] = 0x0F; // No. of Repetitions for Z-Axis = 15
+    i2c_write_blocking(i2c_port, magn_addr_, data, 2, false);
 
-	// Start I2C Transmission
-	Wire1.beginTransmission(BMX055_MAGN_ADDR);
-	// Select Mag register
-	Wire1.write(0x52);
-	// No. of Repetitions for Z-Axis = 15
-	Wire1.write(0x0F);
-	// Stop I2C Transmission
-	Wire1.endTransmission();
-	delay(300);
+    sleep_ms(300); // Delay 300ms to allow the sensor to stabilize
+
+    return 0;
+}
+
+void BMX055::popAccel(float *array)
+{
+    uint8_t reg_addr = 0x02; // Start address for acceleration data
+    uint8_t data[6];         // Buffer to store the acceleration data
+
+    // Read 6 bytes of data from the accelerometer registers
+    int result = i2c_write_blocking(i2c_port, accl_addr_, &reg_addr, 1, true);
+    if (result < 0)
+    {
+        // Handle I2C write error
+        printf("I2C Accel write failed\n");
+        return;
+    }
+
+    result = i2c_read_blocking(i2c_port, accl_addr_, data, 6, false);
+    if (result < 0)
+    {
+        // Handle I2C read error
+        printf("I2C Accel read failed\n");
+        return;
+    }
+
+    // Convert the data to 12-bit values for X, Y, Z
+    int xAccl = ((data[1] << 8) | (data[0] & 0xF0)) >> 4;
+    if (xAccl > 2047)
+        xAccl -= 4096;
+    array[0] = xAccl * accRange;
+
+    int yAccl = ((data[3] << 8) | (data[2] & 0xF0)) >> 4;
+    if (yAccl > 2047)
+        yAccl -= 4096;
+    array[1] = yAccl * accRange;
+
+    int zAccl = ((data[5] << 8) | (data[4] & 0xF0)) >> 4;
+    if (zAccl > 2047)
+        zAccl -= 4096;
+    array[2] = zAccl * accRange;
+    // Calculate total acceleration
+    array[3] = 9.81 * std::sqrt((array[0]) * (array[0]) + (array[1]) * (array[1]) + (array[2]) * (array[2]));
 }
 
 void BMX055::getAcceleration(float *x, float *y, float *z, float *accTotal)
 {
+    float array_[4];
+    popAccel(array_);
+    *x = array_[0];
+    *y = array_[1];
+    *z = array_[2];
+    *accTotal = array_[3];
+}
 
-	for (int i = 0; i < 6; i++)
-	{
-		// Start I2C Transmission
-		Wire1.beginTransmission(BMX055_ACCL_ADDR);
-		// Select data register
-		Wire1.write((2 + i));
-		// Stop I2C Transmission
-		Wire1.endTransmission();
-		// Request 1 byte of data
-		Wire1.requestFrom(BMX055_ACCL_ADDR, 1);
-		// Read 6 bytes of data
-		// xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
-		if (Wire1.available() == 1)
-			_data[i] = Wire1.read();
-	}
+void BMX055::getAcceleration(float *array)
+{
+    float array_[4];
+    popAccel(array_);
+    array[0] = array_[0];
+    array[1] = array_[1];
+    array[2] = array_[2];
+    array[3] = array_[3];
+}
 
-	// Convert the data to 12-bits
-	int xAccl = ((_data[1] * 256) + (_data[0] & 0xF0)) / 16;
-	if (xAccl > 2047)
-		xAccl -= 4096;
-	*x = xAccl * accRange;
-
-	int yAccl = ((_data[3] * 256) + (_data[2] & 0xF0)) / 16;
-	if (yAccl > 2047)
-		yAccl -= 4096;
-	*y = yAccl * accRange;
-
-	int zAccl = ((_data[5] * 256) + (_data[4] & 0xF0)) / 16;
-	if (zAccl > 2047)
-		zAccl -= 4096;
-	*z = zAccl * accRange;
-
-	*accTotal = 9.81 * sqrt((sq(*x) + sq(*y) + sq(*z)));
+void BMX055::getAcceleration(volatile float *array)
+{
+    float array_[4];
+    popAccel(array_);
+    array[0] = array_[0];
+    array[1] = array_[1];
+    array[2] = array_[2];
+    array[3] = array_[3];
 }
 
 float BMX055::getAccelerationX()
 {
-	for (int i = 0; i < 6; i++)
-	{
-		// Start I2C Transmission
-		Wire1.beginTransmission(BMX055_ACCL_ADDR);
-		// Select data register
-		Wire1.write((2 + i));
-		// Stop I2C Transmission
-		Wire1.endTransmission();
-		// Request 1 byte of data
-		Wire1.requestFrom(BMX055_ACCL_ADDR, 1);
-		// Read 6 bytes of data
-		// xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
-		if (Wire1.available() == 1)
-			_data[i] = Wire1.read();
-	}
-
-	// Convert the data to 12-bits
-	int xAccl = ((_data[1] * 256) + (_data[0] & 0xF0)) / 16;
-	if (xAccl > 2047)
-		xAccl -= 4096;
-	float x = xAccl * accRange;
-	return x;
+    float x, y, z, total;
+    getAcceleration(&x, &y, &z, &total);
+    return x;
 }
 
 float BMX055::getAccelerationY()
 {
-	for (int i = 0; i < 6; i++)
-	{
-		// Start I2C Transmission
-		Wire1.beginTransmission(BMX055_ACCL_ADDR);
-		// Select data register
-		Wire1.write((2 + i));
-		// Stop I2C Transmission
-		Wire1.endTransmission();
-		// Request 1 byte of data
-		Wire1.requestFrom(BMX055_ACCL_ADDR, 1);
-		// Read 6 bytes of data
-		// xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
-		if (Wire1.available() == 1)
-			_data[i] = Wire1.read();
-	}
-
-	int yAccl = ((_data[3] * 256) + (_data[2] & 0xF0)) / 16;
-	if (yAccl > 2047)
-		yAccl -= 4096;
-	float y = yAccl * accRange;
-	return y;
+    float x, y, z, total;
+    getAcceleration(&x, &y, &z, &total);
+    return y;
 }
 
 float BMX055::getAccelerationZ()
 {
-	for (int i = 0; i < 6; i++)
-	{
-		// Start I2C Transmission
-		Wire1.beginTransmission(BMX055_ACCL_ADDR);
-		// Select data register
-		Wire1.write((2 + i));
-		// Stop I2C Transmission
-		Wire1.endTransmission();
-		// Request 1 byte of data
-		Wire1.requestFrom(BMX055_ACCL_ADDR, 1);
-		// Read 6 bytes of data
-		// xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
-		if (Wire1.available() == 1)
-			_data[i] = Wire1.read();
-	}
-
-	int zAccl = ((_data[5] * 256) + (_data[4] & 0xF0)) / 16;
-	if (zAccl > 2047)
-		zAccl -= 4096;
-	float z = zAccl * accRange;
-	return z;
+    float x, y, z, total;
+    getAcceleration(&x, &y, &z, &total);
+    return z;
 }
 
 float BMX055::getAccelerationTotal()
 {
-
-	for (int i = 0; i < 6; i++)
-	{
-		// Start I2C Transmission
-		Wire1.beginTransmission(BMX055_ACCL_ADDR);
-		// Select data register
-		Wire1.write((2 + i));
-		// Stop I2C Transmission
-		Wire1.endTransmission();
-		// Request 1 byte of data
-		Wire1.requestFrom(BMX055_ACCL_ADDR, 1);
-		// Read 6 bytes of data
-		// xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
-		if (Wire1.available() == 1)
-			_data[i] = Wire1.read();
-	}
-
-	// Convert the data to 12-bits
-	int xAccl = ((_data[1] * 256) + (_data[0] & 0xF0)) / 16;
-	if (xAccl > 2047)
-		xAccl -= 4096;
-	float x = xAccl * accRange;
-
-	int yAccl = ((_data[3] * 256) + (_data[2] & 0xF0)) / 16;
-	if (yAccl > 2047)
-		yAccl -= 4096;
-	float y = yAccl * accRange;
-
-	int zAccl = ((_data[5] * 256) + (_data[4] & 0xF0)) / 16;
-	if (zAccl > 2047)
-		zAccl -= 4096;
-	float z = zAccl * accRange;
-
-	float accTotal = 9.81 * sqrt((sq(x) + sq(y) + sq(z)));
-	return accTotal;
+    float x, y, z, total;
+    getAcceleration(&x, &y, &z, &total);
+    return total;
 }
 
 void BMX055::getMagnet(int *x, int *y, int *z)
 {
+    uint8_t reg = 0x42;
+    int result = i2c_write_blocking(i2c_port, magn_addr_, &reg, 1, true);
+    if (result < 0)
+    {
+        printf("I2C Mag write failed\n");
+        return;
+    }
+    result = i2c_read_blocking(i2c_port, magn_addr_, (uint8_t *)_data, 6, false);
+    if (result < 0)
+    {
+        printf("I2C Mag read failed\n");
+        return;
+    }
 
-	for (int i = 0; i < 6; i++)
-	{
-		// Start I2C Transmission
-		Wire1.beginTransmission(BMX055_MAGN_ADDR);
-		// Select data register
-		Wire1.write((66 + i));
-		// Stop I2C Transmission
-		Wire1.endTransmission();
-		// Request 1 byte of data
-		Wire1.requestFrom(BMX055_MAGN_ADDR, 1);
-		// Read 6 bytes of data
-		// xMag lsb, xMag msb, yMag lsb, yMag msb, zMag lsb, zMag msb
-		if (Wire1.available() == 1)
-			_data[i] = Wire1.read();
-	}
+    int xMag = ((_data[1] << 8) | (_data[0] & 0xF8)) >> 3;
+    if (xMag > 4095)
+        xMag -= 8192;
+    *x = xMag;
 
-	// Convert the data
-	int xMag = ((_data[1] * 256) + (_data[0] & 0xF8)) / 8;
-	if (xMag > 4095)
-		xMag -= 8192;
-	*x = xMag;
+    int yMag = ((_data[3] << 8) | (_data[2] & 0xF8)) >> 3;
+    if (yMag > 4095)
+        yMag -= 8192;
+    *y = yMag;
 
-	int yMag = ((_data[3] * 256) + (_data[2] & 0xF8)) / 8;
-	if (yMag > 4095)
-		yMag -= 8192;
-	*y = yMag;
-
-	int zMag = ((_data[5] * 256) + (_data[4] & 0xFE)) / 2;
-	if (zMag > 16383)
-		zMag -= 32768;
-	*z = zMag;
+    int zMag = ((_data[5] << 8) | (_data[4] & 0xFE)) >> 1;
+    if (zMag > 16383)
+        zMag -= 32768;
+    *z = zMag;
 }
+
+void BMX055::getMagnet(volatile float *array)
+{
+    uint8_t reg = 0x42;
+    i2c_write_blocking(i2c_port, magn_addr_, &reg, 1, true);
+    i2c_read_blocking(i2c_port, magn_addr_, (uint8_t *)_data, 6, false);
+    int result = i2c_write_blocking(i2c_port, magn_addr_, &reg, 1, true);
+    if (result < 0)
+    {
+        printf("I2C Mag write failed\n");
+        return;
+    }
+    result = i2c_read_blocking(i2c_port, magn_addr_, (uint8_t *)_data, 6, false);
+    if (result < 0)
+    {
+        printf("I2C Mag read failed\n");
+        return;
+    }
+
+    int xMag = ((_data[1] << 8) | (_data[0] & 0xF8)) >> 3;
+    if (xMag > 4095)
+        xMag -= 8192;
+    array[0] = xMag;
+
+    int yMag = ((_data[3] << 8) | (_data[2] & 0xF8)) >> 3;
+    if (yMag > 4095)
+        yMag -= 8192;
+    array[1] = yMag;
+
+    int zMag = ((_data[5] << 8) | (_data[4] & 0xFE)) >> 1;
+    if (zMag > 16383)
+        zMag -= 32768;
+    array[2] = zMag;
+}
+
 void BMX055::getRotation(int *x, int *y, int *z)
 {
+    uint8_t reg = 0x02;
+    int result = i2c_write_blocking(i2c_port, gyro_addr_, &reg, 1, true);
+    if (result < 0)
+    {
+        printf("I2C Gyro write failed\n");
+        return;
+    }
+    result = i2c_read_blocking(i2c_port, gyro_addr_, (uint8_t *)_data, 6, false);
+    if (result < 0)
+    {
+        printf("I2C Gyro read failed\n");
+        return;
+    }
 
-	for (int i = 0; i < 6; i++)
-	{
-		// Start I2C Transmission
-		Wire1.beginTransmission(BMX055_GYRO_ADDR);
-		// Select data register
-		Wire1.write((2 + i));
-		// Stop I2C Transmission
-		Wire1.endTransmission();
-		// Request 1 byte of data
-		Wire1.requestFrom(BMX055_GYRO_ADDR, 1);
-		// Read 6 bytes of data
-		// xGyro lsb, xGyro msb, yGyro lsb, yGyro msb, zGyro lsb, zGyro msb
-		if (Wire1.available() == 1)
-			_data[i] = Wire1.read();
-	}
+    int xGyro = (_data[1] << 8) | _data[0];
+    if (xGyro > 32767)
+        xGyro -= 65536;
+    *x = xGyro;
 
-	// Convert the data
-	int xGyro = (_data[1] * 256) + _data[0];
-	if (xGyro > 32767)
-		xGyro -= 65536;
-	*x = xGyro;
+    int yGyro = (_data[3] << 8) | _data[2];
+    if (yGyro > 32767)
+        yGyro -= 65536;
+    *y = yGyro;
 
-	int yGyro = (_data[3] * 256) + _data[2];
-	if (yGyro > 32767)
-		yGyro -= 65536;
-	*y = yGyro;
+    int zGyro = (_data[5] << 8) | _data[4];
+    if (zGyro > 32767)
+        zGyro -= 65536;
+    *z = zGyro;
+}
 
-	int zGyro = (_data[5] * 256) + _data[4];
-	if (zGyro > 32767)
-		zGyro -= 65536;
-	*z = zGyro;
+void BMX055::getRotation(volatile float *array)
+{
+    uint8_t reg = 0x02;
+    int result = i2c_write_blocking(i2c_port, gyro_addr_, &reg, 1, true);
+    if (result < 0)
+    {
+        printf("I2C Gyro write failed\n");
+        return;
+    }
+    result = i2c_read_blocking(i2c_port, gyro_addr_, (uint8_t *)_data, 6, false);
+    if (result < 0)
+    {
+        printf("I2C Gyro read failed\n");
+        return;
+    }
+
+    int xGyro = (_data[1] << 8) | _data[0];
+    if (xGyro > 32767)
+        xGyro -= 65536;
+    array[0] = xGyro * gyroRes;
+
+    int yGyro = (_data[3] << 8) | _data[2];
+    if (yGyro > 32767)
+        yGyro -= 65536;
+    array[1] = yGyro * gyroRes;
+
+    int zGyro = (_data[5] << 8) | _data[4];
+    if (zGyro > 32767)
+        zGyro -= 65536;
+    array[2] = zGyro * gyroRes;
 }
--- src/BMX055.h
+++ src/BMX055.h
@@ -1,40 +1,46 @@
-#ifndef Morse_h
-#define Morse_h
-
-// the #include statment and code go here...
-
-#endif
 #pragma once
 
-#if (ARDUINO >= 100)
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
+#include <stdio.h>
+#include <cmath>
 
-#include "Wire.h"
+#include "pico/stdlib.h"  // General Pico SDK functions
+#include "hardware/i2c.h" // I2C communication functions
 
-#define BMX055_ACCL_ADDR 0x18
-#define BMX055_GYRO_ADDR 0x68
-#define BMX055_MAGN_ADDR 0x10
+// Define I2C addresses for BMX055 sensor components
+// #define BMX055_ACCL_ADDR 0x18 // SDO1-3v3 0x19 // SDO1-GND address: 0x18
+// #define BMX055_GYRO_ADDR 0x68 // SDO2-3v3 0x69 // SDO2-GND address: 0x68
+// #define BMX055_MAGN_ADDR 0x10 // CSB3-3v3 0x13 // CSB3-GND address: 0x10
+
+#define ACCL_FILTER_ADDR 0x10
 
 class BMX055
 {
 public:
-	BMX055();
-	uint8_t beginAcc(char range);
-	uint8_t beginGyro(void);
-	uint8_t beginMagn(void);
-	void getAcceleration(float *x, float *y, float *z, float *accTotal);
-	float getAccelerationX();
-	float getAccelerationY();
-	float getAccelerationZ();
-	float getAccelerationTotal();
-	void getMagnet(int *x, int *y, int *z);
-	void getRotation(int *x, int *y, int *z);
+    BMX055(i2c_inst_t *port, uint8_t accl_addr, uint8_t gyro_addr, uint8_t magn_addr);
+    uint8_t beginAcc(char range);
+    uint8_t beginGyro(char resolution);
+    uint8_t beginMagn(void);
+    void getAcceleration(float *x, float *y, float *z, float *accTotal);
+    void getAcceleration(float *array);
+    void getAcceleration(volatile float *array); // For working with volatile when doing multicore
+    void popAccel(float *array);
+    float getAccelerationX();
+    float getAccelerationY();
+    float getAccelerationZ();
+    float getAccelerationTotal();
+    void getMagnet(int *x, int *y, int *z);
+    void getMagnet(volatile float *array);
+    void getRotation(int *x, int *y, int *z);
+    void getRotation(volatile float *array);
 
 private:
-	unsigned int _data[6];
-	char _range;
-	float accRange;
+    i2c_inst_t *i2c_port;
+    uint8_t _data[6];
+    char _range;
+    float accRange;
+    char _resolution;
+    float gyroRes;
+    uint8_t accl_addr_; 
+    uint8_t gyro_addr_; 
+    uint8_t magn_addr_;
 };
